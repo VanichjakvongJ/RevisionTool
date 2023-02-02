ff5abc050eb5db420405f0b8ae5c3616e29800cf
Jakpat Vanichjakvong <Vanichjakvong.J@etoncollege.org.uk>
2023-02-02 09:26:23 +0000

    Initial Commit

---
+++ RevisionTool/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RevisionTool
+//
+//  Created by Vanichjakvong, Jakpat (IRG) on 02/02/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RevisionTool/RevisionToolApp.swift
@@ -0,0 +1,17 @@
+//
+//  RevisionToolApp.swift
+//  RevisionTool
+//
+//  Created by Vanichjakvong, Jakpat (IRG) on 02/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct RevisionToolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
